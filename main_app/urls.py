c5d8e5dedc27746c8700b2cfa4f99b68571077ba
Taylor Chin <taylorychin@gmail.com>
2021-07-16 16:33:16 -0700

    created base html files and urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+
+    path('animals/', views.AnimalList.as_view(), name="animals_index"),
+    path('account/signup/', views.signup, name="signup")
 ]
